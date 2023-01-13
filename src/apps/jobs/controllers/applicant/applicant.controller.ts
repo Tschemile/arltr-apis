b3a8bce0f189153257969bb18a0da7f70f03dc6d
chauMinh2510 <chau.pham@terralogic.com>
2023-01-13 10:04:11 +0700

    fix conflict

---
+++ src/apps/jobs/controllers/applicant/applicant.controller.ts
@@ -0,0 +1,80 @@
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query, UseGuards } from "@nestjs/common";
+import { ApiBearerAuth, ApiOkResponse, ApiQuery, ApiTags } from "@nestjs/swagger";
+import { JwtAuthGuard } from "apps/auth";
+import { CreateApplicantDto } from "apps/jobs/dtos/applicant";
+import { GetApplicantsOutput } from "apps/jobs/dtos/applicant/get-applicant.dto";
+import { UpdateApplicantDto } from "apps/jobs/dtos/applicant/update-applicant.dto";
+import { ApplicantService } from "apps/jobs/services/applicant/applicant.service";
+import { HTTP_STATUS } from "utils";
+
+const MODULE_NAME = 'Applicant'
+
+@ApiTags(MODULE_NAME.toLowerCase())
+@Controller(MODULE_NAME.toLowerCase())
+export class ApplicantController {
+    constructor( private readonly applicantService: ApplicantService ){}
+
+    @Post()
+    @UseGuards(JwtAuthGuard)
+    @ApiBearerAuth()
+    @ApiOkResponse({
+      type: CreateApplicantDto,
+        description: 'Successfully Create applicant'
+    })
+    async create(@Body() createApplicantDto: CreateApplicantDto) {
+        return await this.applicantService.create(createApplicantDto);
+    }
+
+    @Get()
+    @UseGuards(JwtAuthGuard)
+    @ApiBearerAuth()
+    @ApiQuery({ name: 'search', required: false })
+    @ApiQuery({ name: 'jobs', required: false, type: [String] })
+    @ApiQuery({ name: 'limit', required: false })
+    @ApiQuery({ name: 'resumes', required: false, type: [String] })
+    async findAll(
+        @Query('search') search,
+        @Query('jobs') jobs,
+        @Query('resumes') resumes,
+        @Query('limit') limit,
+
+    ): Promise<GetApplicantsOutput> {
+        const { applicants, total } = await this.applicantService.findAll({ search, jobs, resumes, limit });
+        return {
+          status: HTTP_STATUS.OK,
+          applicants,
+          total,
+        };
+    }
+
+    @Get(':id')
+    @UseGuards(JwtAuthGuard)
+    @ApiBearerAuth()
+    async findOne(@Param('id') id: string) {
+        return await this.applicantService.findById(id);
+      }
+
+      @Patch(':id')
+      @UseGuards(JwtAuthGuard)
+      @ApiBearerAuth()
+      async update(@Param('id') id: string, @Body() updateApplicantDto: UpdateApplicantDto) {
+        const { status, applicant } = await this.applicantService.update(id, updateApplicantDto);
+        if (status === HTTP_STATUS.Not_Found) {
+          return {
+            status,
+            message: `${MODULE_NAME} not found`,
+          };
+        }
+        return {
+          status,
+          applicant,
+        };
+      }
+
+      @Delete(':id')
+      @UseGuards(JwtAuthGuard)
+      @ApiBearerAuth()
+      async remove(@Param('id') id: string) {
+        return await this.applicantService.remove(id);
+      }
+}
---
+++ src/apps/jobs/dtos/applicant/index.ts
@@ -0,0 +1,4 @@
+export * from './create-applicant.dto'
+export * from './get-applicant.dto'
+export * from './query-applicant.dto'
+export * from './update-applicant.dto'
