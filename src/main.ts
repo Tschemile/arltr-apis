6a8235bc35c8568e488f75966215f0ecd834ebcd
Ren0503 <illuminati0440@gmail.com>
2023-01-15 21:07:17 +0700

    Fix env port

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,15 @@ import { NestFactory } from '@nestjs/core';
 import { SwaggerModule } from '@nestjs/swagger';
 import { swaggerConfig } from 'utils';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config'
+import { config } from 'dotenv'
+
+config()
+const configService = new ConfigService()
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  const PORT = configService.get('PORT') || 5000
   app.enableCors()
   // Validator 
   app.useGlobalPipes(new ValidationPipe());
@@ -18,7 +23,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, swaggerConfig)
   SwaggerModule.setup('docs', app, document)
 
-  await app.listen(5000);
+  await app.listen(PORT);
   console.log(`Application running on ${await (app.getUrl())}`);
 }
 bootstrap();
