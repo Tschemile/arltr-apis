8630c2fb64d34473b483e22c80a1b1bf9ee83d9b
Ren0503 <illuminati0440@gmail.com>
2023-02-03 00:27:49 +0700

    Find user in token decode

--- src/apps/auth/services/auth.service.ts
+++ src/apps/auth/services/auth.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from "@nestjs/common";
 import { JwtService } from "@nestjs/jwt";
-import { Profile } from "apps/profiles";
-import { User } from "apps/users";
+import { Profile, USER_ROLE } from "apps/profiles";
+import { User, UserService } from "apps/users";
 import { UserToken } from "../dtos";
 
 @Injectable()
 export class AuthService {
   constructor(
-    private jwtService: JwtService
+    private jwtService: JwtService,
+    private userService: UserService,
   ) { }
 
   generateToken(user: User, profile: Profile) { 
@@ -20,5 +21,18 @@ export class AuthService {
     return this.jwtService.sign(payload, {
       secret: process.env.JWT_SECRET,
     })
+  }
+
+  async verifyUser(payload: UserToken) {
+    const user = await this.userService.getUserWithProfile(payload.id)
+    const profile = user.profiles.find((x) => x.role === USER_ROLE.USER)
+
+    const format: UserToken = {
+      id: user.id,
+      username: user.username,
+      profile,
+    }
+
+    return format
   }
 }
