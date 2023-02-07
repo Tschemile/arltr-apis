fb449c3022491f892d04b660fae3bada529bebd3
chauMinh2510 <chau.pham@terralogic.com>
2023-02-07 17:13:39 +0700

    fix minor bug

--- src/apps/users/services/verify/verify.service.ts
+++ src/apps/users/services/verify/verify.service.ts
@@ -46,6 +46,10 @@ export class VerifyService extends BaseService<Verify> {
 
     const user = await this.userService.findOne({ email });
 
+    if(!user) {
+      BaseError(TableName.USER, HttpStatus.NOT_FOUND);
+    }
+
     this.mailService.sendMail({
       to: email,
       from: 'pmchauuu@gmail.com',
