6e2a7a809731a34a1d76457349264300ff709e9c
pmchau2510 <75658412+pmchau2510@users.noreply.github.com>
2023-02-07 15:03:20 +0700

    update API verify (#73)
    Co-authored-by: chauMinh2510 <chau.pham@terralogic.com>

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -2,7 +2,7 @@ export function ranDomCode(): string {
     const number = '1234567890';
   
     let code = '';
-    for (let i = 0; i < 5; i++) {
+    for (let i = 0; i < 6; i++) {
       let pos = Math.floor(Math.random() * number.length);
       code += number[pos];
     }
